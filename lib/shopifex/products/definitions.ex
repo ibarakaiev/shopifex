d4adbf25b4b225c02011429794712e80546f200a
Igor Barakaiev <ibarakaiev@gmail.com>
2024-07-23 17:01:27 +0300

    fix: inline `:shopifex` in fetch_env!/2

--- lib/shopifex/products/definitions.ex
+++ lib/shopifex/products/definitions.ex
@@ -1,6 +1,6 @@
 defmodule Shopifex.Products.Definitions do
   def dynamic_products do
-    Application.fetch_env!(Mix.Project.config()[:app], :shopifex_dynamic_products)
+    Application.fetch_env!(:shopifex, :shopifex_dynamic_products)
   end
 
   def dynamic_product_handles do
